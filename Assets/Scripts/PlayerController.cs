1539283c2b19b539dfa128e1b78893bfed3d30f3
Арсен Гаджиев <arsen.gadzhiyev.00@mail.ru>
2025-11-09 14:59:39 +0300

    Final fix bug with animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speedRunMove = 5.0f;
     [SerializeField] private float speedRotate = 15.0f;
 
-    [SerializeField] private float acceleration = 30.0f;
+    [SerializeField] private float acceleration = 25.0f;
 
     [SerializeField] private GameInput gameInput;
 
@@ -15,13 +15,14 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Vector2 inputVector = gameInput.GetInputVector();
-        float speed_move = gameInput.IsRunning() ? speedRunMove : speedMove;
-
         Vector3 move_dir = new(inputVector.x, 0, inputVector.y);
+
+        float speed_move = gameInput.IsRunning() ? speedRunMove : speedMove;
 
         smooth_movement = Vector3.MoveTowards(smooth_movement, move_dir * speed_move, acceleration * Time.deltaTime);
       
         transform.position += smooth_movement * Time.deltaTime;
-        transform.forward = Vector3.Slerp(transform.forward, move_dir, speedRotate * Time.deltaTime);
+        if (move_dir != Vector3.zero)
+            transform.forward = Vector3.Slerp(transform.forward, move_dir, speedRotate * Time.deltaTime);
     }
 }
