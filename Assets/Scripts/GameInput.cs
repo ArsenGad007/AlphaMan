3a04deeb59590b222ea992327938662caadc8609
Арсен Гаджиев <arsen.gadzhiyev.00@mail.ru>
2025-11-09 15:24:58 +0300

    Simplify code and low the run speed player

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -3,17 +3,8 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
-    [SerializeField] private float runDelay = 0.1f; 
-    [SerializeField] private float walkDelay = 0.05f;
-
-    private bool isWalkingKeyPressed = false;
-    private bool isRunKeyPressed = false; 
-
-    private bool isActuallyWalking = false;
-    private bool isActuallyRunning = false;
-
-    private float walkChangeTime;
-    private float runChangeTime;
+    private bool isWalking = false;
+    private bool isRunning = false;
 
     private Vector2 inputVector;
     private PlayerInputActions playerInputActions;
@@ -37,54 +28,21 @@ public class GameInput : MonoBehaviour
         playerInputActions.Player.Run.canceled -= OnRunCanceled;
     }
 
-    private void Update()
-    {
-        UpdateWalking();
-        UpdateRunning();
-    }
-
-    private void UpdateWalking()
-    {
-        if (isWalkingKeyPressed)
-        {
-            if (!isActuallyWalking && Time.time - walkChangeTime >= walkDelay)
-                isActuallyWalking = true;
-        }
-        else if (isActuallyWalking)
-            isActuallyWalking = false;
-    }
-    private void UpdateRunning()
-    {
-        if (isRunKeyPressed && isActuallyWalking)
-        {
-            if (!isActuallyRunning && Time.time - runChangeTime >= runDelay)
-                isActuallyRunning = true;
-        }
-        else if (isActuallyRunning)
-            isActuallyRunning = false;
-    }
-
-
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
         inputVector = context.ReadValue<Vector2>();
-        isWalkingKeyPressed = inputVector != Vector2.zero;
-        walkChangeTime = Time.time;
-
-        // если уже зажат Shift Ч сбрасываем таймер бега, чтобы сработала задержка
-        if (isRunKeyPressed)
-            runChangeTime = Time.time;
+        isWalking = true;
     }
     private void OnMoveCanceled(InputAction.CallbackContext context)
     {
         inputVector = Vector2.zero;
-        isWalkingKeyPressed = false;
+        isWalking = false;
     }
-    private void OnRunPerformed(InputAction.CallbackContext context) => (isRunKeyPressed, runChangeTime) = (true, Time.time);
-    private void OnRunCanceled(InputAction.CallbackContext context) => isRunKeyPressed = false;
+    private void OnRunPerformed(InputAction.CallbackContext context) => isRunning = true;
+    private void OnRunCanceled(InputAction.CallbackContext context) => isRunning = false;
 
 
-    public bool IsWalking() => isActuallyWalking;
-    public bool IsRunning() => isActuallyRunning;
-    public Vector2 GetInputVector() => isActuallyWalking ? inputVector : Vector2.zero;
+    public bool IsWalking() => isWalking;
+    public bool IsRunning() => isWalking && isRunning;
+    public Vector2 GetInputVector() => inputVector;
 }
