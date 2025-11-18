2c3d1e4cbdeb54310114ae3bda5d5cd3421f2669
Smiles-creator <bulavinova07s@gmail.com>
2025-11-19 00:01:56 +0300

    refactor(guard): improve logic field of view visualization

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -1,28 +1,52 @@
 using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
-{
+{ [SerializeField] float viewDistance = 10f;
+  [SerializeField] float fov = 90f;  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-            Mesh mesh = new Mesh();
-            GetComponent<MeshFilter>().mesh = mesh;
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        
+        Vector3 origin = Vector3.zero; 
+        int rayCount = 25;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        
 
-            Vector3[] vertices = new Vector3[3];
-            Vector2[] uv = new Vector2[3];
-            int[] triangles = new int[3];
+        Vector3[] vertices = new Vector3[rayCount + 2];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
 
-            vertices[0] = Vector3.zero;      //центр
-            vertices[1] = new Vector3(5, 0, 1);   // вправо
-            vertices[2] = new Vector3(1, 0, -5);  // назад
+        vertices[0] = origin;
+        int vInd = 1;
+        int tInd = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex = origin + GetVectorFromAngle3D(angle) * viewDistance;
+            vertices[vInd] = vertex;
 
-            triangles[0] = 0;
-            triangles[1] = 1;
-            triangles[2] = 2;
+            if (i > 0)
+            {
+                triangles[tInd + 0] = 0;
+                triangles[tInd + 1] = vInd - 1;
+                triangles[tInd + 2] = vInd;
+                tInd += 3;
+            }
 
-            mesh.vertices = vertices;
-            mesh.uv = uv;
-            mesh.triangles = triangles;
-            mesh.RecalculateNormals();
+            vInd++;
+            angle -= angleIncrease;
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    public static Vector3 GetVectorFromAngle3D(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), 0f, Mathf.Sin(angleRad));
     }
 }
