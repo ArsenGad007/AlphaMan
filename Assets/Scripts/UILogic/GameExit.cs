e4f0113a9393b4b88a48967e49e18cd9c3cc79e1
Arsen Gadzhiyev <arsen.gadzhiyev.00@mail.ru>
2026-03-25 23:31:43 +0300

    Add saves

--- Assets/Scripts/UILogic/GameExit.cs
+++ Assets/Scripts/UILogic/GameExit.cs
@@ -1,10 +1,20 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using NaughtyAttributes;
+
+public enum GameExitPlace
+{
+    Menu,
+    Output
+}
 
 public class GameExit : MonoBehaviour
 {
-    [SerializeField] private GameObject gameExitPanel;  // Ссылка на панель выхода;
+    [ShowIf("gameExitPlace", GameExitPlace.Menu)][SerializeField] private GameInput gameInput;
+    [SerializeField] private GameExitPlace gameExitPlace;   // Куда нужно выйти;
+    [SerializeField] private GameObject gameExitPanel;      // Ссылка на панель выхода;
     [SerializeField] private Button exitButton;
     [SerializeField] private Button yesButton;
     [SerializeField] private Button noButton;
@@ -40,6 +50,15 @@ public class GameExit : MonoBehaviour
 
     private void YesButton()
     {
+        if(gameExitPlace == GameExitPlace.Menu)
+        {
+            Time.timeScale = 1;
+            gameInput.DisablePlayerInputActions();
+            gameExitPanel.SetActive(false);
+            SceneManager.LoadScene("LevelSelect");
+            return;
+        }
+
         Debug.Log("Выход из игры");
 
         // Для редактора Unity
