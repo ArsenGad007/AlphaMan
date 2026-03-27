fdcd297d42dfa7334f8fca94822ef5db21b1e825
Arsen Gadzhiyev <arsen.gadzhiyev.00@mail.ru>
2026-03-27 16:15:15 +0300

    Refactor code and add delete saves button

--- Assets/Scripts/DeleteSaves.cs
+++ Assets/Scripts/DeleteSaves.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class DeleteSaves : MonoBehaviour
+public class GameDeleteSaves : MonoBehaviour
 {
+    private Button button;
+
     void Start()
     {
-        PlayerPrefs.DeleteAll();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
     }
+
+    private void OnClick()
+    {
+        PlayerPrefs.DeleteAll();
+        if (SceneManager.GetActiveScene().name == "LevelSelect")
+        {        
+            var chooseLevel = FindFirstObjectByType<GameSaveLoad>();
+            chooseLevel.UpdateButtons();
+        }
+        else if (SceneManager.GetActiveScene().name != "StartMenu")
+        {
+            Time.timeScale = 1;
+            SceneTransition.Load("LevelSelect");
+        }         
+    }   
 }
