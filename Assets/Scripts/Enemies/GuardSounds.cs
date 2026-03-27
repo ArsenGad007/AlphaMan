596abf4c406ff8d00e6badd1d8ad95ad6ba394c5
Smiles-creator <bulavinova07s@gmail.com>
2026-03-27 03:29:08 +0300

    distribute guards

--- Assets/Scripts/Enemies/GuardSounds.cs
+++ Assets/Scripts/Enemies/GuardSounds.cs
@@ -4,8 +4,8 @@ public class GuardSounds : MonoBehaviour
 {
     [SerializeField] public AudioClip[] gruntSounds;
     [Range(0f, 1f)] private float groanVolume = 1f;
-    private float minInterval = 15f;  
-    private float maxInterval = 25f;
+    private float minInterval = 10f;  
+    private float maxInterval = 50f;
     private float nextGroanTime;
 
     void Start()
